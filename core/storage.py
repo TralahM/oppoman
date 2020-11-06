75eb4e8d8e44a256b377c2185a075db688e150cb
TralahM <musyoki.brian@tralahtek.com>
2020-11-06 23:04:10 +0300

    whitenoise middleware
    Signed-off-by: TralahM <musyoki.brian@tralahtek.com>

--- core/storage.py
+++ core/storage.py
@@ -2,5 +2,5 @@ from whitenoise.storage import CompressedManifestStaticFilesStorage
 
 
 class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    # manifest_strict = False
-    pass
+    manifest_strict = False
+    # pass
